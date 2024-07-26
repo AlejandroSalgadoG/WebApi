53423d805db39d6388696f19f6ad1339baecfa4e
Alejandro Salgado <alejando.salgado@benchling.com>
2024-07-26 09:49:15 -0500

    add custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,27 @@
-from django.db import models  # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    # password is set to none to allow the creation of an unusable user for testing
+    # this is the default behavior of the django user model
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=email, **kwargs)  # create new user object
+        user.set_password(password)  # hash password
+        user.save(using=self._db)  # best practice is to select database (usefull when working with multiple db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
