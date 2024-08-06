9b52ef31ad8f16e2af19dd717a42aac20dfa72ec
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-06 16:50:13 -0500

    add user creation to script

--- web_api_requests.py
+++ web_api_requests.py
@@ -11,3 +11,20 @@ SERVER_URL = 'http://localhost:8000'
 EMAIL = "request@example.com"
 NAME = "request user"
 PASSWORD = "test123"
+
+
+################### create user ###################
+logging.info("start creation of new user")
+payload = {
+    "email": EMAIL,
+    "password": PASSWORD,
+    "name": NAME,
+}
+response = requests.post(f"{SERVER_URL}/api/user/create/", json=payload)
+content = response.json()
+
+assert response.status_code == 201
+assert len(content) == 2
+assert content["email"] == EMAIL
+assert content["name"] == NAME
+logging.info("new user created")
