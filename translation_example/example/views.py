b092bec7950d67df7d92453554a25e4afe59e8c2
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-07 14:59:56 -0500

    add translation

--- translation_example/example/views.py
+++ translation_example/example/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 def index(request):
     context = {
-        "hello": "Hello",
+        "hello": _("Hello"),
     }
     return render(request, "index.html", context)
