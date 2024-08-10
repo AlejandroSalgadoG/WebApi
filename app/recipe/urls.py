ec82a8a3645ffb5c9db48d3d4fa7d6e3ef63c067
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-09 19:00:23 -0500

    add list tag functionality

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register("recipes", views.RecipeViewSet)  # automatically create routes based on RecipeViewSet
+router.register("tags", views.TagViewSet)
 
 app_name = "recipe"
 
