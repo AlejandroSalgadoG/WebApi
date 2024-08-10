e6cbdc2cd4b7df74d95725e49ae40b31b9cec998
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-10 12:18:29 -0500

    add list ingredients

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,7 @@ from recipe import views
 router = DefaultRouter()
 router.register("recipes", views.RecipeViewSet)  # automatically create routes based on RecipeViewSet
 router.register("tags", views.TagViewSet)
+router.register("ingredients", views.IngredientViewSet)
 
 app_name = "recipe"
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
 
 
@@ -35,6 +35,18 @@ class TagViewSet(
 ):
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+class IngredientViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
