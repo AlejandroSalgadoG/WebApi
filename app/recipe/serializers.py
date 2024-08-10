f03d642f4bd7ee2738655d5d1b7188bf84211a03
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-10 11:26:17 -0500

    add update tag from recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -18,18 +18,32 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
-        # by default nested serializers are read only
-        # overwrite behavior to create tags
+    def _get_or_create_tags(self, tags, recipe):
         auth_user = self.context["request"].user
-
-        tags = validated_data.pop("tags", [])
-        recipe = Recipe.objects.create(**validated_data)
         for tag in tags:
             tag_obj, _ = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        # by default nested serializers are read only
+        # overwrite behavior to create tags
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        # overwrite behavior to update tags
+        tags = validated_data.pop("tags", None)  # patch might not include tags to be modified
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
