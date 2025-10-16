a237ae27c6f7da9231fcd7ae4e8752437b74b5ab
Alejandro Salgado <asalg@softserveinc.com>
2025-10-16 14:50:33 -0500

    implement user token generation endpoint

--- src/user/views.py
+++ src/user/views.py
@@ -1,7 +1,12 @@
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import generics
 
-from user.serializers import UserSerializer
+from user.serializers import AuthTokenSerializer, UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
