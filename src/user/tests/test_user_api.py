6e70b2086a1488c939e3685f7aa3a39d0f98f1e1
Alejandro Salgado <asalg@softserveinc.com>
2025-10-16 14:56:20 -0500

    add token creation tests

--- src/user/tests/test_user_api.py
+++ src/user/tests/test_user_api.py
@@ -6,10 +6,11 @@ from rest_framework.test import APIClient
 
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 def create_user(**kwargs):
-    return get_user_model().objects.create(**kwargs)
+    return get_user_model().objects.create_user(**kwargs)
 
 
 def get_payload() -> dict[str, str]:
@@ -49,3 +50,25 @@ class PublicUserApiTests(TestCase):
 
         user_query = get_user_model().objects.filter(username=payload["username"])
         self.assertFalse(user_query.exists())
+
+    def test_create_token_for_user(self):
+        payload = get_payload()
+
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        good_payload = get_payload()
+        create_user(**good_payload)
+
+        bad_payload = good_payload.copy()
+        bad_payload["password"] = "bad_pass"
+
+        res = self.client.post(TOKEN_URL, bad_payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
