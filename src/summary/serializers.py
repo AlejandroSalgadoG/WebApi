75e43e2486976d9d63b0e0e67962ef51a07497e5
Alejandro Salgado <asalg@softserveinc.com>
2025-10-17 09:50:06 -0500

    redefine summary as autogenerated

--- src/summary/serializers.py
+++ src/summary/serializers.py
@@ -3,8 +3,23 @@ from rest_framework import serializers
 from core.models import Summary
 
 
+def generate_summary(text: str) -> str:
+    return text
+
+
 class SummarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Summary
         fields = ["id", "text", "summary"]
-        read_only_fields = ["id"]
+        read_only_fields = ["id", "summary"]
+
+    def create(self, validated_data):
+        text = validated_data.get("text", "")
+        validated_data["summary"] = generate_summary(text)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        text = validated_data.get("text", instance.text)
+        if text != instance.text:
+            validated_data["summary"] = generate_summary(text)
+        return super().update(instance, validated_data)
